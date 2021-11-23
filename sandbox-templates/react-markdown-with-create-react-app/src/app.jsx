c4bda0d880fcdef446964e38cebfe1c7bb684523
Christian Murphy <christian.murphy.42@gmail.com>
2021-11-23 11:52:48 -0700

    docs: describe how to update dependencies on stackblitz

--- sandbox-templates/react-markdown-with-create-react-app/src/app.jsx
+++ sandbox-templates/react-markdown-with-create-react-app/src/app.jsx
@@ -1,5 +1,7 @@
 // ⚠️ Important! Please make sure the dependencies are up to date.
-// You can refresh them in the Dependencies section (left-bottom on CodeSandbox)
+// On code sandbox, you can refresh them in the Dependencies section (left-bottom)
+// On stackblitz, you can open the package.json file, update the versions,
+// then run npm install in the stackblitz terminal
 
 import React from 'react'
 import Markdown from 'react-markdown'
